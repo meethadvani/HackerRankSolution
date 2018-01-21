91e75d35292e53279703b8fe78a5d047438e6cca
meet <meethadvani@gmail.com>
2018-01-21 13:41:51 +0530

    Search partial ContactName from given contactList

---
+++ src/hackerrank/ContactSearch.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ *
+ * @author meet
+ */
+public class ContactSearch {
+
+    public static HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+    public static void addContact(String name) {
+
+        for (int i = 0; i < name.length(); i++) {
+            String partialName = name.substring(0, i);
+            if (map.containsKey(partialName)) {
+                int frequency = map.get(partialName);
+                map.put(partialName, ++frequency);
+            } else {
+                map.put(partialName, 1);
+            }
+        }
+    }
+
+    public static int findContact(String name) {
+        if (map.containsKey(name)) {
+            int ans = map.get(name);
+            return ans;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList a = new ArrayList<>();
+        for (int a0 = 0; a0 < n; a0++) {
+            String op = in.next();
+            String contact = in.next();
+            if (op.equals("add")) {
+                addContact(contact);
+            } else if (op.equals("find")) {
+                a.add(findContact(contact));
+            }
+        }
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i));
+        }
+    }
+}
