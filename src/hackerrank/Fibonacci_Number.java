dd8a4670fdeee44c0317354767431edefd04e700
meet <meethadvani@gmail.com>
2018-02-25 17:50:19 +0530

    Improved version of fibonacci

--- src/hackerrank/Fibonacci_Number.java
+++ src/hackerrank/Fibonacci_Number.java
@@ -12,10 +12,13 @@ import java.util.Scanner;
  * @author meet
  */
 //Question Link : https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
-
 public class Fibonacci_Number {
 
+    static int count = 0;
+    static int count1 = 0;
+
     public static int fibonacci(int n) {
+        //System.out.println("Count: " + count++);
         int result;
         // Complete the function.
         // return (n > 2) ? fibonacci(n - 1) + fibonacci(n - 2) : 1; (when 0 < n )
@@ -30,11 +33,26 @@ public class Fibonacci_Number {
         return result;
     }
 
+    //Without recursion
+    public static int fibonacciWithoutRec(int n) {
+        int result = 0;
+        int end = 1;
+        int start = 0;
+        for (int i = 1; i < n; i++) {
+            //System.out.println("Count WR: " + count1++);
+            result = start + end;
+            start = end;
+            end = result;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
-        System.out.println(fibonacci(n));
+        System.out.println("Using recursion: " + fibonacci(n));
+        System.out.println("Withot recursion: " + fibonacciWithoutRec(n));
     }
 
 }
