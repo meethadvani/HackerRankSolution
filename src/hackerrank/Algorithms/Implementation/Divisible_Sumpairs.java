7f26569610a0d59b5a0dba48e5cc7ab5035e80f9
meet <meethadvani@gmail.com>
2018-03-11 16:01:52 +0530

    Divisible sum pair

---
+++ src/hackerrank/Algorithms/Implementation/Divisible_Sumpairs.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank.Algorithms.Implementation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author meet
+ */
+//Question Link : https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+public class Divisible_Sumpairs {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        // Complete this function
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((ar[i] + ar[j]) % k == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for (int ar_i = 0; ar_i < n; ar_i++) {
+            ar[ar_i] = in.nextInt();
+        }
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+
+}
